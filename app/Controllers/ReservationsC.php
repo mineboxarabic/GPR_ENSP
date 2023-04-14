52e8fec6e0fd42e8dba537e0a2e0d1942e3c7e60
mineboxarabic <81491887+mineboxarabic@users.noreply.github.com>
2023-04-14 14:49:56 +0200

    test

---
+++ app/Controllers/ReservationsC.php
@@ -0,0 +1,6 @@
+<?php 
+
+class ReservationsC extends Controller
+{
+    
+}
--- public/assets/js/costomJS.js
+++ public/assets/js/costomJS.js
@@ -3,19 +3,21 @@ import * as jQuery from './jquery-2.2.4.min.js' ;
 
 function autocompletIndexUser(){
     var min_length = 0; // min caracters to display the autocomplete
-    var keyword = $('.form-control').val();
-    if (keyword.length >= min_length) {
+    var keyword = $('#user_id').val();
+    console.log(keyword);
+    if (keyword.length > min_length) {
         $.ajax({
-            url: 'app/Controllers/autocompleteIndexUser.php',
+            url: 'autocomplete/getData/'+keyword,
             type: 'POST',
             data: {keyword:keyword},
             success:function(data){
-                //$('#user_list_id').show();
+                $('#user_list_id').show();
                 $('#user_list_id').html(data);
+                $('#user_list_id .list-group').css('position','absolute');
             }
         });
     } else {
-        $('#search_index_list_id').hide();
+        $('#user_list_id').hide();
     }
 }
 
@@ -23,4 +25,5 @@ $(document).ready(function(){
     $('#user_id').keyup(function(){
         autocompletIndexUser();
     });
+
 });
